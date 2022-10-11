780b299dca1780510f696619855fc787e804062c
Nitko Nitkić <nitkonitkic@Nitkos-MacBook-Air.local>
2022-10-11 20:21:14 +0200

    Initial pom.xml setup.

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # oprpp
+
+
+hr.fer.oprpp1.jmbag0036534699
+
+hw01-0036534699
---
+++ hw01-0036534699/src/test/java/hr/fer/oprpp1/jmbag0036534699/AppTest.java
@@ -0,0 +1,20 @@
+package hr.fer.oprpp1.jmbag0036534699;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
